73834faf780a594484f2eee62fd0f323e7fddbed
user-1 <user-1@MacBook-Pro.local>
2020-07-31 15:46:36 -0600

    server set up

--- server/controller.js
+++ server/controller.js
@@ -1,15 +1,25 @@
-const books = require('./books.json');
+let books = require('./books.json');
 let nextId = 21;
 
 module.exports = {
     getBooks: (req, res) => {
         res.status(200).send(books)
     },
+    //Check to make sure getGenre is right. I changed books up above to be let instead of const, and now it's working in postman.
+    getGenre: (req, res) => {
+        const {genre} = req.query
+        // console.log(req.query)
+        let filteredGenre = books.filter(book => book.genre === genre)
+        console.log(filteredGenre)
+        books = filteredGenre
+        res.status(200).send(books)
+    },
     addBook: (req, res) => {
+        // console.log(req.body)
         const {title, author, genre, coverImg} = req.body;
-        // if(!title) {
-        //     res.status(405).send('Expected to receive book info')
-        // } else {
+        if(!title || !author || !genre) {
+            res.status(405).send('Expected to receive complete book info')
+        } else {
             const newBook = {
                 id: nextId,
                 title,
@@ -21,13 +31,26 @@ module.exports = {
             nextId++;
             books.push(newBook);
             res.status(200).send(books)
-            console.log(newBook)
-        // }
+            // console.log(newBook)
+        }
     },
-//     deleteBook: (req, res) => {
-//         res.status(200).send(books)
-//     },
-//     completeBook: (req, res) => {
-//         res.status(200).send(books)
-//     }
+    //Once again - check deleteBook. I made books above let instead of const and it's working in Postman.
+    deleteBook: (req, res) => {
+        const {id} = req.params
+        books = books.filter(book => book.id !== +id )
+        // console.log(req.params)
+        res.status(200).send(books)
+        
+    },
+    completeBook: (req, res) => {
+        const {id} = req.params
+        // console.log(req.params)
+        const index = books.findIndex(book => book.id === +id)
+        books[index].completed = true
+        console.log(books[index])
+        res.status(200).send(books)
+    }
 }
+
+// With complete book, I'm trying to
+//find id of book that is completed. If the id in the array books matches the id in the req.query, change status from completed false to completed true. There will be a toggle function in react for this with conditional rendering.
--- server/index.js
+++ server/index.js
@@ -6,9 +6,10 @@ const ctrl = require('./controller');
 app.use(express.json());
 
 app.get('/api/books', ctrl.getBooks)
+app.get('/api/books/genre', ctrl.getGenre)
 app.post('/api/books', ctrl.addBook)
-// app.delete('/api/books/:id', ctrl.deleteBook)
-// app.put('/api/books/complete/:id', ctrl.completeBook)
+app.delete('/api/books/:id', ctrl.deleteBook)
+app.put('/api/books/complete/:id', ctrl.completeBook)
 
 
 app.listen(port, () => 
